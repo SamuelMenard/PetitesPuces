9fa4347a6eca5d6affb014e52e48436b79e5e7c2
SamuelMenard <samuel.menard@videotron.ca>
2019-01-23 22:37:14 -0500

    login redirections

--- Pages/Connexion.aspx.cs
+++ Pages/Connexion.aspx.cs
@@ -11,4 +11,44 @@ public partial class Pages_Connexion : System.Web.UI.Page
     {
 
     }
+
+    public void inscriptionClient_click(Object sender, EventArgs e)
+    {
+        String url = "~/Pages/InscriptionClient.aspx?";
+        Response.Redirect(url, true);
+    }
+
+    public void inscriptionVendeur_click(Object sender, EventArgs e)
+    {
+        String url = "~/Pages/InscriptionVendeur.aspx?";
+        Response.Redirect(url, true);
+    }
+
+    public void connexion_click(Object sender, EventArgs e)
+    {
+        String courriel = tbCourriel.Text;
+        String MDP = tbMDP.Text;
+
+        if (courriel == "client")
+        {
+            Session["TypeUtilisateur"] = "C";
+            /*String url = "~/Pages/InscriptionVendeur.aspx?";
+            Response.Redirect(url, true);*/
+
+        }
+        else if (courriel == "vendeur")
+        {
+            Session["TypeUtilisateur"] = "V";
+            /*String url = "~/Pages/InscriptionVendeur.aspx?";
+            Response.Redirect(url, true);*/
+
+        }
+        else if (courriel == "gestionnaire")
+        {
+            Session["TypeUtilisateur"] = "G";
+            String url = "~/Pages/AcceuilGestionnaire.aspx?";
+            Response.Redirect(url, true);
+        }
+        
+    }
 }
