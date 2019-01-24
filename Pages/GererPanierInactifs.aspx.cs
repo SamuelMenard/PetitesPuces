655b20567c1d2c3b5540ecd7eb7d6bcd2bd69a69
Franc <Franc@LAPTOP-FP>
2019-01-24 13:57:00 -0500

    Consultation / GererPanierInactifs

--- Pages/GererPanierInactifs.aspx.cs
+++ Pages/GererPanierInactifs.aspx.cs
@@ -7,26 +7,33 @@ using System.Web.UI.WebControls;
 
 public partial class Pages_GererPanierInactifs : System.Web.UI.Page
 {
-    List<String> lstImages = new List<string>();
-    String nomEntreprise = "Apple";
-    Panel panelBody;
     DropDownList ddlNbMois;
     int moisChoisis;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        getNbMois();         
+        creerPage();
+    }
+
 
     protected void Page_LoadComplete(object sender, EventArgs e)
     {
         ddlNbMois.SelectedValue = this.moisChoisis.ToString();
     }
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        getNbMois();  
 
+    private void creerPage()
+    {
+        List<String> lstImages = new List<string>();
+        String nomEntreprise = "Apple";
+        Panel panelBody;
+        
         Panel panelGroup = LibrairieControlesDynamique.divDYN(phDynamique, nomEntreprise + "_PanelGroup", "panel-group");
         Panel panelBase = LibrairieControlesDynamique.divDYN(panelGroup, nomEntreprise + "_base", "panel panel-default");
         // Nom de l'entreprise
         Panel panelHeader = LibrairieControlesDynamique.divDYN(panelBase, nomEntreprise + "_header", "panel-heading");
 
-        Panel rowInactif = LibrairieControlesDynamique.divDYN(panelHeader, nomEntreprise + "_rowInactif_", "row");       
+        Panel rowInactif = LibrairieControlesDynamique.divDYN(panelHeader, nomEntreprise + "_rowInactif_", "row");
         Panel colInactif = LibrairieControlesDynamique.divDYN(rowInactif, nomEntreprise + "_colInactif_", "col-sm-6");
         LibrairieControlesDynamique.lblDYN(colInactif, nomEntreprise + "_nom", nomEntreprise, "nom-entreprise");
         Panel colDDLInactif = LibrairieControlesDynamique.divDYN(rowInactif, nomEntreprise + "_colDDLInactif_", "col-sm-6");
@@ -34,9 +41,9 @@ public partial class Pages_GererPanierInactifs : System.Web.UI.Page
         ddlNbMois = LibrairieControlesDynamique.ddlDYN(colDDLInactif, "ddlMoisInactifs", "left15");
         ddlNbMois.Items.Clear();
         ddlNbMois.AutoPostBack = true;
-        ddlNbMois.ViewStateMode = ViewStateMode.Enabled;
-        ddlNbMois.SelectedIndexChanged += valeursPanier; 
-        ddlNbMois.AppendDataBoundItems = true;
+        //ddlNbMois.ViewStateMode = ViewStateMode.Enabled;
+        ddlNbMois.SelectedIndexChanged += valeursPanier;
+        //ddlNbMois.AppendDataBoundItems = true;
         ddlNbMois.Items.Insert(0, new ListItem(String.Empty, String.Empty));
         ddlNbMois.Items.Insert(1, new ListItem("1", "1"));
         ddlNbMois.Items.Insert(2, new ListItem("2", "2"));
@@ -62,19 +69,13 @@ public partial class Pages_GererPanierInactifs : System.Web.UI.Page
             lstImages.Add("350,97;Homepod - Blanc x1;../static/images/orangeShop.png");
         }
 
-        creerPage();
-    }
-
-    private void creerPage()
-    {        
         panelBody.Controls.Clear();
         LibrairieControlesDynamique.hrDYN(panelBody, "OrangeBorder", 30);
         // Rajouter les produits dans le panier
         int idItem = 0;
         for (int i = 0; i < 2; i++)
         {
-            idItem++;
-            int quantiteSelectionne = 1;
+            idItem++;            
             string[] strValeurs;
             Double prix = 0.0;
             String nomProduit = "";
@@ -124,7 +125,7 @@ public partial class Pages_GererPanierInactifs : System.Web.UI.Page
 
     private void btnSupprimer_click(object sender, EventArgs e)
     {
-        throw new NotImplementedException();
+        
     }
 
     private void valeursPanier(object sender, EventArgs e)
