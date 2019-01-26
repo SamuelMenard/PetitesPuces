baa8914615e5ffe1c087aec5227a1f04e7746dbf
Olivier Brodeur <olivierbrodeur@hotmail.com>
2019-01-26 11:17:09 -0500

    Add libraire courriel testée + envoi courriel confirmation inscription client

--- Pages/InscriptionClient.aspx.cs
+++ Pages/InscriptionClient.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Mail;
 
 public partial class Pages_InscriptionClient : System.Web.UI.Page
 {
@@ -29,13 +30,53 @@ public partial class Pages_InscriptionClient : System.Web.UI.Page
             client.DateCreation = DateTime.Now;
 
             dbContext.PPClients.Add(client);
-            dbContext.SaveChanges();
 
-            tbCourriel.Text = "";
-            tbConfirmationCourriel.Text = "";
-            lblMessage.Text = "Le profil à été créé";
-            divMessage.CssClass = "alert alert-success alert-margins";
-            divMessage.Visible = true;
+            bool binOK = true;
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                binOK = false;
+            }
+
+            if (binOK)
+            {
+                MailMessage message = new MailMessage("ppuces@gmail.com", client.AdresseEmail);
+                message.Subject = "Création profil Les Petites Puces";
+                message.Body = string.Format("Bonjour,\n\n" +
+                                             "Suite à votre demande, votre profil Les Petites Puces a été créé. Votre numéro de client est {0}. Voici vos informations de connexion :\n" +
+                                             "Identifiant : {1}\n" +
+                                             "Mot de passe : {2}\n\n" +
+                                             "Merci de faire affaire avec nous,\n" +
+                                             "Les Petites Puces",
+                                             client.NoClient, 
+                                             client.AdresseEmail, 
+                                             client.MotDePasse);
+
+                if (LibrairieCourriel.envoyerCourriel(message))
+                {
+                    tbCourriel.Text = "";
+                    tbConfirmationCourriel.Text = "";
+                    lblMessage.Text = "Votre profil à été créé. Un rappel de vos informations de connexion vous a été envoyé par courriel.";
+                    divMessage.CssClass = "alert alert-success alert-margins";
+                    divMessage.Visible = true;
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                tbCourriel.Text = "";
+                tbConfirmationCourriel.Text = "";
+                lblMessage.Text = "Le profil n'a pas été créé";
+                divMessage.CssClass = "alert alert-danger alert-margins";
+                divMessage.Visible = true;
+            }
         }
     }
 }
